c1d9125d4c9e45ca9056765d5edcc6f373b5cfa3
laryssa <laryss.moraes@gmail.com>
2023-06-13 20:47:23 -0300

    adiciionar/remover tarefas

--- src/pages/HomeList.jsx
+++ src/pages/HomeList.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+import { listaTarefas, removeTarefa } from "../services/TaskService"
+
+
 export default function HomeList() {
+  const [tarefas, setTarefas] = useState([])      /*  novo */
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    async function carrega() {
+      setLoading(true)
+      const data = await listaTarefas()    /*  novo */
+      setTarefas(data)
+      setLoading(false)
+      
+    }
+    carrega()
+  },[])
+  
+  async function handleClick(key){
+    await removeTarefa(key)
+  }
+
   return (
     <>
+    {loading? <h3>Aguarde...</h3>:        /*  novo */
+    <ol>
+      {tarefas.map((tarefa, key) =>
+        <li key={key}>{tarefa.nome} - {tarefa.prioridade}
+          <button onClick={() => handleClick(tarefa.key) }>Remover</button>
+        </li>)}
+    </ol>
+}
     </>
   )
 }
--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,19 +1,34 @@
-import { UrlAPI } from "./FirebaseConfig";
+import { urlApi } from './FirebaseConfig'
 
-export async function listaTarefa () {
-    await fetch (UrlAPI)
-    .then((response => response.json))
-    .then((data) => console.log(data))
-    .catch((error) => {throw Error ('Tá errado! arruma isso aí...')}) 
-}
-
-export async function insereTarefa(tarefa) {
-    await fetch(UrlAPI, {
-        method: 'POST',
-        body: JSON.stringify(),
-        headers: {
-            'Content-type': 'application/json'
+export async function listaTarefas(){
+    let tarefas =[]                       /*  novo */
+    await  fetch(urlApi + "tarefas.json")               
+    .then((response)=> response.json())
+    .then((data) => {      /*  novo */
+        for(let key in data){      /*  novo */
+            tarefas.push({key, ...data[key]})
         }
     })
-    .catch((error) => {throw Error('Errou feio, errou rude...')})
-} 
+    .catch((error) => {throw Error("Deu ruim")})
+    return tarefas
+}
+
+export async function insereTarefa(tarefa){
+   await fetch(urlApi + "tarefas.json",{
+        method: "POST",
+        body: JSON.stringify(tarefa),
+        headers:{
+            'Content-type': 'application/json'}
+        
+    })
+    .catch((error) => {throw Error("Deu ruim")})
+}
+
+
+export async function removeTarefa(key){
+    await fetch(urlApi+ "tarefas/" + key + ".json", {
+        method:'DELETE'
+    })
+
+.catch((error) => {throw Error("Deu ruim")})
+}
